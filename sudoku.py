8aaced72e92ba252500dbc000e493f40759f6f15
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-06-06 22:15:45 +1000

    initial

---
+++ sudoku.py
@@ -0,0 +1,109 @@
+SUDOKU1 = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+
+SUDOKU2 = [
+    [8, 2, 7, 1, 5, 4, 3, 9, 6],
+    [9, 6, 5, 3, 2, 7, 1, 4, 8],
+    [3, 4, 1, 6, 8, 9, 7, 5, 2],
+    [5, 9, 3, 4, 6, 8, 2, 7, 1],
+    [4, 7, 2, 5, 1, 3, 6, 8, 9],
+    [6, 1, 8, 9, 7, 2, 4, 3, 5],
+    [7, 8, 6, 2, 3, 5, 9, 1, 4],
+    [1, 5, 4, 7, 9, 6, 8, 2, 3],
+    [2, 3, 9, 8, 4, 1, 5, 6, 7]
+]
+
+def sudoku_is_solved(board: list[list[int]]):
+    """
+    Check if a sudoku has been solved.
+    """
+
+    # check rows
+    for i in range(9):
+        if len(set(board[i])) != 9:
+            print("fail 1")
+            print(board[i])
+            return False
+    
+    # check columns
+    for i in range(9):
+        if len(set([board[x][i] for x in range(9)])) != 9:
+            print("fail 2")
+            return False
+        
+    # check squares
+    for i in range(3):
+        for j in range(3):
+            s = set()
+            for k in range(3):
+                for l in range(3):
+                    s.add(board[3*j+l][3*i+k])
+            if len(s) != 9:
+                print("fail 3")
+                return False
+    
+    return True
+
+def is_valid(board: list[list[int]], i: int, j: int, a: int) -> bool:
+    """
+    Check if a particular number at a certain square is valid.
+    """
+
+    # check the row
+    if a in set(board[j]):
+        return False
+    
+    # check the column
+    if a in set([board[y][i] for y in range(9)]):
+        return False
+    
+    # check the squares
+    rstart = j // 3
+    cstart = i // 3
+    s = set()
+    for r in range(3):
+        for c in range(3):
+            if a == board[3*cstart + c][3*rstart + r]:
+                return False
+    return True
+            
+            
+
+
+def solve_sudoku(sudoku: list[list[int]]):
+    for i in range(9):
+        for j in range(9):
+            if sudoku[j][i] != 0:
+                continue
+
+            for a in range(1, 10):
+                if is_valid(sudoku, i, j, a):
+                    sudoku[j][i] = a
+                    solve_sudoku(board)
+                    sudoku[j][i] = 0
+
+            
+
+print(sudoku_is_solved(SUDOKU1))
+print(sudoku_is_solved(SUDOKU2))
+
+
+def is_valid(board: list[list[int]], x: int, y: int) -> bool:
+    board[y][x]
+
+def find_sol(board):
+    for x in range(9):
+        for y in range(9):
+            if board[y][x] == 0:
+                continue
+            for j in range(1, 10):
+                print(j)
