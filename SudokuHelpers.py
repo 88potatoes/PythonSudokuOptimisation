d0a5ec5734e91f086f136f315848a339acff701c
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-06-15 17:06:37 +1000

    add get_n_empty_squares

--- SudokuHelpers.py
+++ SudokuHelpers.py
@@ -6,17 +6,19 @@ def number_is_valid(board: list[list[int]], r: int, c: int, a: int) -> bool:
     # check the row
     if a in set(board[r]):
         return False
-    
+
     # check the column
     if a in set([board[y][c] for y in range(9)]):
         return False
-    
+
     # check the squares
     r_start = r // 3
     c_start = c // 3
     for dr in range(3):
         for dc in range(3):
-            if a == board[3*r_start + dr][3*c_start + dc]:
+            if a == board[3 * r_start + dr][3 * c_start + dc]:
                 return False
     return True
 
+def get_n_empty_squares(sudoku: list[list[int]]) -> int:
+    return sum(1 for row in sudoku for cell in row if cell == 0)
