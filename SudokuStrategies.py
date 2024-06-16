5fbb43fdaf29ee9e7c26238549c54061f0e4be1d
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2024-06-16 15:25:01 +1000

    change to accommodate different sudokus sizes

--- SudokuStrategies.py
+++ SudokuStrategies.py
@@ -2,25 +2,26 @@ import copy
 from SudokuHelpers import number_is_valid
 
 
-def basic_backtracking(sudoku: list[list[int]]) -> list[list[int]] | None:
+def basic_backtracking(sudoku: list[list[int]], size: int = 3) -> list[list[int]] | None:
     """
-        Basic backtracking approach to solve sudoku.
-        """
+    Basic backtracking approach to solve sudoku.
+    """
     sudokucopy = copy.deepcopy(sudoku)
 
     solution = None
+    side_length = size ** 2
 
     def dfs():  # TODO can optimise by not looping through everything again
         nonlocal solution
         all_filled = True
-        for r in range(9):
-            for c in range(9):
+        for r in range(side_length):
+            for c in range(side_length):
                 if sudokucopy[r][c] != 0:
                     continue
 
                 all_filled = False
-                for a in range(1, 10):
-                    if number_is_valid(sudokucopy, r, c, a):
+                for a in range(1, side_length + 1):
+                    if number_is_valid(sudokucopy, r, c, a, sudoku_size=size):
                         sudokucopy[r][c] = a
                         if dfs():
                             return True
